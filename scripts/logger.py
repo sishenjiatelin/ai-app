263baf6b991c31f104b4c6ac8b30d5f1b5429aa4
sishenjiatelin <2987379200@qq.com>
2026-04-27 16:29:55 +0800

    log 第二次修改，增加对网络不稳定的鲁棒性，就加循环，多试几次

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,5 +1,6 @@
 import requests
 import logging
+import time
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = BASE_DIR/ "data" / "input.txt"
@@ -11,11 +12,23 @@ try:
         text = f.read()
     print("内容：",text)
     logging.info(f"输入：{text}")
-    url = "https://httpbin.org/post"
-    response = requests.post(url,data={"content":text},timeout=5)
-    response.raise_for_status()
-    print("服务器回给我的是：", response.text)
-    logging.info(f"输出：{response.text}")
+    # url = "https://httpbin.org/post"
+    url = "https://httpbin.org/status/500"
+    success = False
+    for i in range(3):
+        try:
+            response = requests.post(url,data={"content":text},timeout=5)
+            response.raise_for_status()
+            print("第", i + 1, "次请求成功")
+            logging.info(f"第 {i + 1} 次请求成功，输出：{response.text}")
+            success = True
+            break
+        except Exception as e:
+            print("第", i + 1, "次请求失败：", e)
+            logging.error(f"第 {i + 1} 次请求失败：{e}")
+            time.sleep(1)
+    if not success:
+        logging.error("请求失败：已经重试 3 次，程序结束")
 except Exception as e:
     print("程序出错了：", e)
     logging.error(f"错误：{e}")
