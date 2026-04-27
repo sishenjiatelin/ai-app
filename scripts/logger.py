feeb62dfd04333bb7278c6a9e6d5af55ee77b4ab
sishenjiatelin <2987379200@qq.com>
2026-04-27 15:47:28 +0800

    log 第一次修改，加入logging，和try，except

--- scripts/logger.py
+++ scripts/logger.py
@@ -1,14 +1,21 @@
 import requests
+import logging
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = BASE_DIR/ "data" / "input.txt"
-LOG_DIR = BASE_DIR/ "logs" / "log.txt"
-with open(DATA_DIR,"r",encoding="utf-8")as f:
-    text = f.read()
-print("内容：",text)
-url = "https://httpbin.org/post"
-response = requests.post(url,data={"content":text})
-print("服务器回给我的是：", response.text)
-with open(LOG_DIR,"a",encoding="utf-8")as f:
-    f.write("输入："+ "\n" + text+ "\n")
-    f.write("输出：" +  "\n" + response.text + "\n")
+LOG_FILE = BASE_DIR/ "logs" / "log.txt"
+logging.basicConfig(filename = LOG_FILE,level = logging.INFO,
+format = "%(asctime)s | %(levelname)s |%(message)s",encoding="utf-8")
+try:
+    with open(DATA_DIR,"r",encoding="utf-8")as f:
+        text = f.read()
+    print("内容：",text)
+    logging.info(f"输入：{text}")
+    url = "https://httpbin.org/post"
+    response = requests.post(url,data={"content":text},timeout=5)
+    response.raise_for_status()
+    print("服务器回给我的是：", response.text)
+    logging.info(f"输出：{response.text}")
+except Exception as e:
+    print("程序出错了：", e)
+    logging.error(f"错误：{e}")
