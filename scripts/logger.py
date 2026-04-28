cf524472d82fa823770e74622e025172e21b771e
sishenjiatelin <2987379200@qq.com>
2026-04-28 15:23:12 +0800

    log日志文件第四次修改，模块化处理

--- scripts/logger.py
+++ scripts/logger.py
@@ -5,33 +5,55 @@ from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parents[1]
 DATA_DIR = BASE_DIR/ "data" / "input.txt"
 LOG_FILE = BASE_DIR/ "logs" / "log.txt"
+url_post = "https://httpbin.org/post"
+url_test = "https://httpbin.org/status/500"
 logging.basicConfig(filename = LOG_FILE,level = logging.INFO,
 format = "%(asctime)s | %(levelname)s |%(message)s",encoding="utf-8")
-try:
-    with open(DATA_DIR,"r",encoding="utf-8")as f:
+
+def LogConfig():
+    logging.basicConfig(filename = LOG_FILE,level = logging.INFO,
+    format = "%(asctime)s | %(levelname)s | %(message)s",
+    encoding = "utf-8")
+
+def READ_Text():
+    with open(DATA_DIR,"r",encoding = "utf-8")as f:
         text = f.read()
     print("内容：",text)
     logging.info(f"输入：{text}")
-    url = "https://httpbin.org/post"
-    # url = "https://httpbin.org/status/500"
-    success = False
+    return text
+
+def Call_API(url,text):
     for i in range(3):
         try:
-            response = requests.post(url,data={"content":text},timeout=5)
-            data =response.json()
+            respose = request.post(url,data = {"content":text},timeout = 5)
+            respose.raise_for_status()
+            data = respose.json()
             content = data["form"]["content"]
-            response.raise_for_status()
-            print("第", i + 1, "次请求成功")
-            print("接口收到的内容是：", content)
-            logging.info(f"第 {i + 1} 次请求成功，输出：{content}")
-            success = True
-            break
+            print(f"第{i+1}次失败")
+            logging.info(f"第{i+1}次成功,输出{content}")
+            return content
         except Exception as e:
-            print("第", i + 1, "次请求失败：", e)
-            logging.error(f"第 {i + 1} 次请求失败：{e}")
+            logging.error(f"第{i+1}次请求失败：{e}")
             time.sleep(1)
-    if not success:
-        logging.error("请求失败：已经重试 3 次，程序结束")
-except Exception as e:
-    print("程序出错了：", e)
-    logging.error(f"错误：{e}")
+    logging.error(f"第三次失败,请求失败！")
+    return None
+    
+def main():
+    try:
+        LogConfig()
+
+        text = READ_Text()
+        content = Call_API(url_test,text)
+
+        if content is None:
+            print("请求失败，请查看日志")
+        else:
+            print("接口收到的内容是：", content)
+
+    except Exception as e:
+        print("程序出错了：", e)
+        logging.error(f"程序失败：{e}")
+
+
+if __name__ == "__main__":
+    main()
